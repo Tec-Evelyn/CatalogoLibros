72593ea91b4ce22f49adbf56946ab616f6f99402
Evelyn <evelynliquez04@outlook.com>
2023-09-13 16:42:47 -0600

    Programacion de la clase Program capa WebAPI

--- CatalogoLibros.WebAPI/Program.cs
+++ CatalogoLibros.WebAPI/Program.cs
@@ -33,9 +33,57 @@ builder.Services.AddSwaggerGen(c =>
 
     c.AddSecurityDefinition(jwtSecurityScheme.Reference.Id, jwtSecurityScheme);
     c.AddSecurityRequirement(new OpenApiSecurityRequirement { { jwtSecurityScheme, Array.Empty<string>() } });
+    // ******************************************
 });
 
+// *** Reducir el tamaño del JSON quitando las propiedades nulas o con valores por defecto ***
+builder.Services.AddControllers()
+   .AddJsonOptions(options =>
+   {
+       options.JsonSerializerOptions.IgnoreNullValues = true;
+       options.JsonSerializerOptions.WriteIndented = true;
+   });
+//****************************************************************
+
+#region Seguridad por token JWT
+var key = "8c8fbc6b-f62c-4100-9faf-3808590e57b8"; // Agregar la llave
+
+// Configurar el JWT
+builder.Services
+ .AddAuthentication(x =>
+ {
+     // Configurar la autentificaion de JWT por defecto en la Web API
+     x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+     x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+ })
+ .AddJwtBearer(x =>
+ {
+     // Agregar las configuracion por defecto al JWT
+     x.RequireHttpsMetadata = false;
+     x.SaveToken = true;
+     x.TokenValidationParameters = new TokenValidationParameters
+     {
+         IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(key)),
+         ValidateAudience = false,
+         ValidateIssuerSigningKey = true,
+         ValidateIssuer = false
+     };
+ });
+
+// Aplicar la inyeccion de dependencia para JWT
+builder.Services.AddSingleton<IJwtAuthenticatioService>(new JwtAuthenticatioService(key));
+#endregion
+
 var app = builder.Build();
+
+// Habilitar los Cors para todo tipo de origenes
+app.UseCors(options =>
+{
+    options.WithOrigins("*");
+    options.AllowAnyMethod();
+    options.AllowAnyHeader();
+});
+//*****************************
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -45,6 +93,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication(); //agregar la autenticación
 
 app.UseAuthorization();
 
