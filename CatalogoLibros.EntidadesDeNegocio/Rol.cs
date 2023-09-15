1daedea480a06e160c92c145ea23e119bb2884a4
Evelyn <evelynliquez04@outlook.com>
2023-09-14 21:06:08 -0600

    Modificacion de las clases Autor y Rol capa EntidadesDeNegocios

--- CatalogoLibros.EntidadesDeNegocio/Rol.cs
+++ CatalogoLibros.EntidadesDeNegocio/Rol.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -19,6 +20,7 @@ namespace CatalogoLibros.EntidadesDeNegocio
 
         [NotMapped]
         public int Top_Aux { get; set; }
+        [ValidateNever]
         public List<Usuario> Usuario { get; set; }
     }
 }
